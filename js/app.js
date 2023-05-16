ef690a2f536c857dedce53631c5cb7cbab4628f7
Hector Lopez <hlopdia699@g.educaand.es>
2023-05-16 12:38:16 +0200

    validacion terminada

--- js/app.js
+++ js/app.js
@@ -8,16 +8,9 @@ var contrasena = document.getElementById("contrasena").value;
 var contrasena2 = document.getElementById("contrasena2").value;
 var acepto = document.getElementById("acepto").checked;
 
-
-// Swal.fire({
-//     title: 'Error!',
-//     text: 'Do you want to continue',
-//     icon: 'error',
-//     confirmButtonText: 'Cool'
-//   })
-
+/* variables creadas para la validación */
+var todoOk = true;
 var infoUsuario = [nombre, usuario, email, contrasena, contrasena2];
-
 
 infoUsuario.forEach(dato => {
 
@@ -29,7 +22,8 @@ infoUsuario.forEach(dato => {
             text: 'Quedan campos vacíos',
             icon: 'error',
             confirmButtonText: 'Ups!'
-          })    
+          })
+        todoOk = false;  
     }
 })
 
@@ -42,6 +36,7 @@ if(contrasena != contrasena2)
         icon: 'error',
         confirmButtonText: 'Ouch!'
       })
+      todoOk = false;  
 }
 
 if(!acepto)
@@ -52,6 +47,30 @@ if(!acepto)
         text: 'Acepta las condiciones!',
         icon: 'error',
         confirmButtonText: '¡Voy!'
+      })
+      todoOk = false;  
+}
+
+/* Creo el objeto JS con las variables que voy a enviar: */
+var registroJS =
+{
+    nombre,
+    usuario,
+    email,
+    contrasena,
+};
+/* Lo convierto a Json: */
+var salidaJSON = JSON.stringify(registroJS);
+
+/* Si todas las validaciones han funcionado lo muestro en pantalla */
+if(todoOk)
+{
+    Swal.fire({
+        title: 'Registrado!',
+        text: salidaJSON,
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 2000
       })
 }
 
